1f7e782b8ad7201c7316aaa92d0e093409749771
Neko Of The Abyss <psi@innocent.com>
2022-08-27 00:40:25 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # moe-counter
 Counter inspired by [journey-ad/Moe-counter](https://github.com/journey-ad/Moe-counter)
+
+### Usage
+
+Start the project:
+
+```
+deno task start
+```
+
+This will watch the project directory and restart as necessary.
---
+++ routes/get/[name]/json.ts
@@ -0,0 +1,9 @@
+import { HandlerContext } from "$fresh/server.ts";
+import db from "../../../utils/sqlite.ts"
+
+export const handler = (_req: Request, ctx: HandlerContext): Response => {
+  const name = ctx.params.name;
+  const data = db.get(name);
+  db.set(name, data.count+1)
+  return new Response(JSON.stringify(data));
+};
