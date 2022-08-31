6e3e9103913382c3cae5aa8e8d0095446738d4d3
Neko Of The Abyss <psi@innocent.com>
2022-08-31 15:35:02 +0530

    add github endpoints

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,14 +4,16 @@
 
 import * as $0 from "./routes/get/[name]/img.ts";
 import * as $1 from "./routes/get/[name]/json.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $2 from "./routes/github/[name]/[data].ts";
+import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/get/[name]/img.ts": $0,
     "./routes/get/[name]/json.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/github/[name]/[data].ts": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/github/[name]/[data].ts
@@ -0,0 +1,82 @@
+import { HandlerContext } from "$fresh/server.ts";
+
+import { generate } from "../../../utils/generate.ts";
+
+interface RepoData {
+  stargazers_count: number;
+  size: number;
+  watchers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  forks: number;
+  open_issues: number;
+  watchers: number;
+}
+
+interface UserData {
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+}
+
+export const handler = async (
+  req: Request,
+  ctx: HandlerContext,
+): Promise<Response> => {
+  const name = ctx.params.name;
+  let theme = new URL(req.url).searchParams.get("theme");
+  if (!theme) theme = "gelbooru";
+  let n = 0;
+  switch (ctx.params.data) {
+    case "stars": {
+      const res = await fetch(`https://api.github.com/users/${name}/repos`);
+      if (res.status == 200) {
+        n = await res.json().then((x) =>
+          (x as RepoData[]).reduce(
+            (acc: number, repo) => acc + repo.stargazers_count,
+            0,
+          )
+        );
+      }
+      break;
+    }
+    case "followers": {
+      {
+        const res = await fetch(`https://api.github.com/users/${name}`);
+        if (res.status == 200) {
+          n = await res.json().then(x => (x as UserData).followers)
+        }
+        break;
+      }
+    }
+    case "following": {
+      const res = await fetch(`https://api.github.com/users/${name}`);
+      if (res.status == 200) {
+        n = await res.json().then(x => (x as UserData).following)
+      }
+      break;
+    }
+    case "gists": {
+      const res = await fetch(`https://api.github.com/users/${name}`);
+      if (res.status == 200) {
+        n = await res.json().then(x => (x as UserData).public_gists)
+      }
+      break;
+    }
+    case "repos":
+    default: {
+      const res = await fetch(`https://api.github.com/users/${name}`);
+      if (res.status == 200) {
+        n = await res.json().then(x => (x as UserData).public_repos)
+      }
+      break;
+    }
+  }
+  return new Response(generate(n, theme), {
+    headers: {
+      "Content-Type": "image/svg+xml",
+      "cache-control": "max-age=0, no-cache, no-store, must-revalidate",
+    },
+  });
+};
