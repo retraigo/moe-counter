c24cfbb7f55740bd13bddb6e003ba53b3a22cb88
NeTT <nett@nett.moe>
2024-04-02 14:14:28 +0530

    minor changes to website

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -4,16 +4,61 @@ import { useState } from "preact/hooks";
 import { tw } from "@twind";
 
 import { Button } from "../components/Button.tsx";
-export default function Counter() {
-  const [n, setn] = useState("foo");
-  let name = n;
-  return (
-    <div class={tw`flex flex-col space-y-8 w-full`}>
-      <div class = {tw`flex flex-row items-center`}><span>https://count.nett.moe/get/</span><input class = {tw`p-1 border border-gray-600`} type = "text" onInput={e => name = e.currentTarget.value} value={name} /><span>/img</span></div>
-      <button class = {tw`p-2 bg-gray-300`} onClick={e => setn(name)}>Test</button>
 
+type Theme =
+  | "gelbooru"
+  | "asoul"
+  | "gelbooru-h"
+  | "moebooru"
+  | "moebooru-h"
+  | "rule34";
+
+const options: Theme[] = [
+  "asoul",
+  "gelbooru",
+  "gelbooru-h",
+  "moebooru",
+  "moebooru-h",
+  "rule34",
+];
+
+export default function Counter() {
+  const [n, setn] = useState<{ name: string; theme: Theme }>({
+    name: "foo",
+    theme: "gelbooru",
+  });
+  let name = n.name;
+  let theme = n.theme;
+  return (
+    <div class={tw`flex flex-col space-y-8 w-full `}>
+      <div class={tw`md:flex flex-row items-center gap-2`}>
+        <div class={tw`md:flex flex-row items-center bg-gray-900 text-white p-3`}>
+          <span>https://count.nett.moe/get/</span>
+          <input
+            class={tw`p-1 w-24 border border-gray-600 bg-gray-800 font-mono`}
+            type="text"
+            onInput={(e) => name = e.currentTarget.value}
+            value={name}
+          />
+          <span>/img?theme=</span>
+          <select class={tw`p-1 border border-gray-600 bg-gray-800 font-mono`} onChange={(e) => theme = e.currentTarget.value as Theme}>
+            {options.map((opt) => <option value={opt} selected={opt === "gelbooru"}>{opt}</option>)}
+          </select>
+        </div>
+        <div class={tw`flex gap-2`}>
+        <button
+          class={tw`p-2 bg-gray-300`}
+          onClick={(e) => {
+            setn({ name, theme });
+          }}
+        >
+          Test
+        </button>
+        <button class={tw`p-2 bg-gray-300`} onClick={e => navigator.clipboard.writeText(`https://count.nett.moe/get/${name}/img?theme=${theme}`) }>Copy</button>
+        </div>
+      </div>
       <p class={tw`flex-grow-1 font-bold text-xl`}>Image</p>
-      <img src={`/get/${n}/img`} />
+      <img src={`/get/${n.name}/img?theme=${n.theme}`} />
     </div>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -21,9 +21,6 @@ export default function Home() {
         Front page WIP. Check out the below tho:
       </p>
       <Counter />
-      <div class = {tw`mt-6 w-full`}>
-        <div class = {tw`mx-auto text-center bg-gray-900 text-white p-1`} onCopy = {e => e.clipboardData?.setData("text/plain", "https://count.nett.moe/get/CUSTOM_NAME/img?theme=gelbooru") }>https://count.nett.moe/get/CUSTOM_NAME/img?theme=gelbooru | asoul | gelbooru-h | moebooru | moebooru-h | rule34 </div>
-      </div>
       <a class = {tw`mt-6 block w-full`} href="https://github.com/retraigo/moe-counter">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
           <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
