503ae5f031482dbe3bbec3033e780fa5def2271a
NeTT <nett@nett.moe>
2024-12-17 08:04:11 +0530

    add fallback

--- routes/get/[name]/img.ts
+++ routes/get/[name]/img.ts
@@ -6,7 +6,12 @@ export const handler = async (req: Request, ctx: HandlerContext): Promise<Respon
   const name = ctx.params.name;
   let theme = new URL(req.url).searchParams.get("theme");
   if (!theme) theme = "gelbooru";
-  const data = await db.get(["count", name]);
-  db.set(["count", name], (Number(data.value) || 0) + 1);
-  return new Response(generate((Number(data.value) || 0), theme), {headers: {"Content-Type": "image/svg+xml", 'cache-control': 'max-age=0, no-cache, no-store, must-revalidate'}});
+  try {
+    const data = await db.get(["count", name]);
+    db.set(["count", name], (Number(data.value) || 0) + 1);
+    return new Response(generate((Number(data.value) || 0), theme), {headers: {"Content-Type": "image/svg+xml", 'cache-control': 'max-age=0, no-cache, no-store, must-revalidate'}});  
+  } catch (e) {
+    console.error(e)
+    return new Response(generate((999999 || 0), theme), {headers: {"Content-Type": "image/svg+xml", 'cache-control': 'max-age=0, no-cache, no-store, must-revalidate'}});
+  }
 };
